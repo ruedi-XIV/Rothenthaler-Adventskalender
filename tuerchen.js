f1f508e113d0dab4e0ee4660ce57c0cf692838ba
ruedi.XIV <ruediger.ulbricht@freenet.de>
2026-09-07 22:12:05 +0200

    Update tuerchen.js

--- tuerchen.js
+++ tuerchen.js
@@ -28,7 +28,7 @@ const tuerchenDaten = [
     { position: 18, nummer: 6,  video: "eurMlBedWnY" },
 
     { position: 19, nummer: 5,  video: "JMmIsGf-Npc" },
-    { position: 20, nummer: 16, video: "" },
+    { position: 20, nummer: 16, video: "L3blOp-BBE0" },
     { position: 21, nummer: 10, video: "nKcHmgw3tqA" },
     { position: 22, nummer: 23, video: "" },
     { position: 23, nummer: 13, video: "n78Rru3oAZI" },
